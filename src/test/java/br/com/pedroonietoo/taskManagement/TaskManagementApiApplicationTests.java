b8385d99c65916c451dca1f20c520fb711fcbcbd
Pedro Nieto <pedronieto.2005@gmail.com>
2024-01-07 18:57:54 -0300

    First commit

---
+++ src/test/java/br/com/pedroonietoo/taskManagement/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroonietoo.taskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
