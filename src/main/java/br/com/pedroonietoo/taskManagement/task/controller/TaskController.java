8629ab450c71b70e084a94f6c7fcca5ffb1fbfa0
Pedro Nieto <pedronieto.2005@gmail.com>
2024-01-07 20:06:29 -0300

    docs: added Swagger documentation

--- src/main/java/br/com/pedroonietoo/taskManagement/task/controller/TaskController.java
+++ src/main/java/br/com/pedroonietoo/taskManagement/task/controller/TaskController.java
@@ -1,9 +1,17 @@
 package br.com.pedroonietoo.taskManagement.task.controller;
 
+import br.com.pedroonietoo.taskManagement.TaskManagementApiApplication;
+import br.com.pedroonietoo.taskManagement.errors.ErrorResponse;
 import br.com.pedroonietoo.taskManagement.task.dto.TaskDto;
 import br.com.pedroonietoo.taskManagement.task.model.TaskModel;
 import br.com.pedroonietoo.taskManagement.task.repository.TaskRepository;
 import br.com.pedroonietoo.taskManagement.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Tag(name = "Task", description = "Everything aboyt tasks")
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -26,6 +35,17 @@ public class TaskController {
     @Autowired
     private UserRepository userRepository;
 
+    @Operation(
+            summary = "Creates a task",
+            description = "Creates a task in the database.",
+            tags = {"Task"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Task created successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "User not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @PostMapping("/")
     public ResponseEntity createTask(@RequestBody @Valid TaskDto taskDto) {
         var task = new TaskModel();
@@ -50,11 +70,30 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskRepository.save(task));
     }
 
+    @Operation(
+            summary = "Lists all tasks",
+            description = "Lists all tasks in the database.",
+            tags = {"Task"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Tasks listed successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @GetMapping("/")
     public ResponseEntity<List<TaskModel>> getTasks() {
         return ResponseEntity.status(HttpStatus.OK).body(taskRepository.findAll());
     }
 
+    @Operation(
+            summary = "List a task by id",
+            description = "Lists a specific task information by passing the id.",
+            tags = {"Task"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Task information listed successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Object> getTaskById(@PathVariable(name = "id") UUID id) {
         var task = taskRepository.findById(id);
@@ -64,6 +103,17 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
+    @Operation(
+            summary = "Updates a user's information",
+            description = "Updates a user's information in the database.",
+            tags = {"Task"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Task information updated successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateTask(@PathVariable(name = "id") UUID id, @RequestBody @Valid TaskDto taskDto) {
         Optional<TaskModel> task = taskRepository.findById(id);
@@ -83,6 +133,16 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(taskRepository.save(taskData));
     }
 
+    @Operation(
+            summary = "Deletes a task",
+            description = "Removes a task in the database.",
+            tags = {"Task"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Task removed successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Task not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteTask(@PathVariable(name = "id") UUID id) {
         var task = taskRepository.findById(id);
--- src/main/java/br/com/pedroonietoo/taskManagement/user/controller/UserController.java
+++ src/main/java/br/com/pedroonietoo/taskManagement/user/controller/UserController.java
@@ -1,9 +1,17 @@
 package br.com.pedroonietoo.taskManagement.user.controller;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.pedroonietoo.taskManagement.TaskManagementApiApplication;
+import br.com.pedroonietoo.taskManagement.errors.ErrorResponse;
 import br.com.pedroonietoo.taskManagement.user.dto.UserDto;
 import br.com.pedroonietoo.taskManagement.user.model.UserModel;
 import br.com.pedroonietoo.taskManagement.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +25,7 @@ import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Tag(name = "User", description = "Everything aboyt users")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,6 +33,17 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Operation(
+            summary = "Creates a user",
+            description = "Creates a user in the database.",
+            tags = {"User"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User created successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "409", description = "User with same e-mail already exists", content = {@Content(schema = @Schema(), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @PostMapping("/")
     public ResponseEntity<Object> createUser(@RequestBody @Valid UserDto userDto) {
         var userModel = new UserModel();
@@ -41,12 +61,31 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.save(userModel));
     }
 
+    @Operation(
+            summary = "Lists all users",
+            description = "Lists all users in the database.",
+            tags = {"User"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Users listed successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @GetMapping("/")
     public ResponseEntity<List<UserModel>> getUsers() {
         List<UserModel> usersList = userRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(usersList);
     }
 
+    @Operation(
+            summary = "List a user by id",
+            description = "Lists a specific user information by passing the id.",
+            tags = {"User"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User information listed successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Object> getUserById(@PathVariable(value = "id") UUID id) {
         Optional<UserModel> user = userRepository.findById(id);
@@ -56,6 +95,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(user.get());
     }
 
+    @Operation(
+            summary = "Updates a user's information",
+            description = "Updates a user's information in the database.",
+            tags = {"User"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User information updated successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "409", description = "User with same e-mail already exists", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateUser(@PathVariable(name = "id") UUID id, @RequestBody @Valid UserDto userDto) {
         Optional<UserModel> user = userRepository.findById(id);
@@ -69,6 +120,16 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userData));
     }
 
+    @Operation(
+            summary = "Deletes a user",
+            description = "Removes a user in the database.",
+            tags = {"User"}
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User removed successfully", content = {@Content(schema = @Schema(implementation = TaskManagementApiApplication.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error | Something went wrong", content = {@Content(schema = @Schema(implementation = ErrorResponse.class), mediaType = "application/json")})
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteUser(@PathVariable(name = "id") UUID id) {
         var user = userRepository.findById(id);
